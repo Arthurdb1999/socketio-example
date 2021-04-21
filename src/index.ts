e43bead43b78e35c9fc738288708dfa7cb0a8980
arthurdb1999 <arthurdb1999@gmail.com>
2021-04-21 20:00:04 -0300

    DONE: websocket demo

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import socketio from 'socket.io'
+import path from 'path'
+import http from 'http'
+
+const app = express()
+const httpServer = http.createServer(app)
+const io = new socketio.Server(httpServer)
+
+app.use(express.static(path.resolve(__dirname, '..', 'public')))
+
+io.on('connection', socket => {
+    console.log(`New connection: ${socket.id}`)
+
+    socket.on('message', message => {
+        socket.emit('received', `Received message ${message}`)
+        //io.emit ou socket.broadcast para enviar a todos clientes conectados
+    })
+})
+
+httpServer.listen(3333)
